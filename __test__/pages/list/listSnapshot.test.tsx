abef24afdd312fedcd35b713037a2fceab319746
DumakIt <odumak4411@gmail.com>
2023-07-25 22:58:39 +0900

    230725 스냅샷 테스트 render 수정

--- __test__/pages/list/listSnapshot.test.tsx
+++ __test__/pages/list/listSnapshot.test.tsx
@@ -1,15 +1,29 @@
 import { render } from "@testing-library/react";
 import "@testing-library/jest-dom";
 import { RecoilRoot } from "recoil";
-import ApolloSetting from "../../../src/commons/apollo";
 import ListPage from "../../../pages/list";
+import {
+  ApolloClient,
+  ApolloProvider,
+  HttpLink,
+  InMemoryCache,
+} from "@apollo/client";
+import fetch from "cross-fetch";
 
 it("list 페이지 snapshot 테스트", () => {
+  const client = new ApolloClient({
+    link: new HttpLink({
+      uri: "http://mock.com/graphql",
+      fetch,
+    }),
+    cache: new InMemoryCache(),
+  });
+
   const result = render(
     <RecoilRoot>
-      <ApolloSetting>
+      <ApolloProvider client={client}>
         <ListPage />
-      </ApolloSetting>
+      </ApolloProvider>
     </RecoilRoot>
   );
   expect(result.container).toMatchSnapshot();
