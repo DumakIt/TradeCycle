5585f4355f88f7fd702a490c18afb50f2c9fc89a
DumakIt <odumak4411@gmail.com>
2023-07-26 22:14:02 +0900

    230726 list 페이지 테스트 코드 작성완료

--- jest.config.js
+++ jest.config.js
@@ -15,8 +15,6 @@ const customJestConfig = {
   moduleDirectories: ["node_modules", "<rootDir>/"],
   testEnvironment: "jest-environment-jsdom",
   setupFilesAfterEnv: ["./jest.setup.js"],
-  errorOnDeprecated: true,
-  collectCoverage: true,
 };
 
 // createJestConfig is exported this way to ensure that next/jest can load the Next.js config which is async
