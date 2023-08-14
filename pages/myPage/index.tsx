ff6e62cb5a2802fc97bc3c3d2ca0bd4e92f67404
DumakIt <odumak4411@gmail.com>
2023-08-14 21:04:48 +0900

    230814 마이페이지 권한분기 추가

--- pages/myPage/index.tsx
+++ pages/myPage/index.tsx
@@ -1,5 +1,8 @@
+import { useWithAuth } from "../../src/components/commons/hooks/custom/useWithAuth";
 import UsedMarketMyPage from "../../src/components/units/usedMarket/myPage/usedMarketMyPage";
 
 export default function MyPage(): JSX.Element {
+  useWithAuth();
+
   return <UsedMarketMyPage />;
 }
